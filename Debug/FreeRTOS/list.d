e06dfb789713ebc886952111971896574cdd2f8e
Bonnie Huang <molk3206@gmail.com>
2024-06-22 18:18:12 +0800

    first commit

---
+++ Debug/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/FreeRTOS.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/FreeRTOSConfig.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/projdefs.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/portable.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/deprecated_definitions.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/mpu_wrappers.h \
+ /Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/list.h
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/FreeRTOS.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/FreeRTOSConfig.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/projdefs.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/portable.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/deprecated_definitions.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/mpu_wrappers.h:
+/Users/CK/STM32CubeIDE/workspace_1.11.0/lab_1/FreeRTOS/include/list.h:
